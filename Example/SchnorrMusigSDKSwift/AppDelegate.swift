0c94a7856bea61216c7e95ec92ee109be514aa94
Niraj Modi <nirajmodi@Nirajs-MacBook-Air.local>
2024-01-03 09:45:26 +0530

    Initial commit

---
+++ Example/SchnorrMusigSDKSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SchnorrMusigSDKSwift
+//
+//  Created by docusingndev112 on 01/03/2024.
+//  Copyright (c) 2024 docusingndev112. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/SchnorrMusigSDKSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SchnorrMusigSDKSwift
+//
+//  Created by docusingndev112 on 01/03/2024.
+//  Copyright (c) 2024 docusingndev112. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
